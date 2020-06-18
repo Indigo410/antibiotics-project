e5c70450e26819d4167e5ee408c5a5f5e9355ce5
Cecilia Xiao <yux164@ucsd.edu>
2020-06-17 18:14:16 -0700

    Revert "removed old files"
    This reverts commit 3811047f2eb0a9b9c90ca1aaac7669c6f4ca56eb.

---
+++ old_scripts_and_notebooks/mediation_simulation.py
@@ -0,0 +1,140 @@
+import numpy as np
+from scipy.linalg import eigh
+
+
+def _A(n,pairs=((1,0),(3,2))):
+    """
+    helper method for producing the cov matrix A
+    for multivariate mediation simulation
+    """
+    A=np.zeros((n,n))
+    np.fill_diagonal(A,1)
+    for i,j in pairs:
+        A[i,j]=1
+        A[j,i]=1
+    return A
+
+   
+def simple_mediation(n, beta11 = -2, beta21 = 2, b32 = -1.5):
+    producer = np.random.rand(n)
+    mm = beta21*producer + np.random.normal(size=n)
+    target = beta11*producer + b32*mm + np.random.normal(size=n)
+
+    otu = np.vstack([producer,target])
+    temp = {"OTU": otu,"Metabolite": np.expand_dims(mm, axis=0)}
+
+    return temp
+    
+def simple_mediation_direct(n, beta11 = -2, beta21 = 2, b32 = -1.5):
+    producer = np.random.rand(n)
+    mm = beta21*producer + np.random.normal(size=n)
+    target = b32*mm + np.random.normal(size=n)
+
+    otu = np.vstack([producer,target])
+    temp = {"OTU":otu,"Metabolite":np.expand_dims(mm, axis=0)}
+
+    return temp
+
+def simulate_simple_mediation(n, p_otu, p_metabolite, mediations = 1,mediation_func=simple_mediation_direct):
+    not_mediation_otu = p_otu-2*mediations
+    not_mediation_metabolite = p_metabolite-mediations
+
+    otutable = np.random.rand(not_mediation_otu,n)
+    metabolitetable = np.random.rand(not_mediation_metabolite,n)
+
+    for _ in range(mediations):
+        temp = mediation_func(n)
+        otutable = np.vstack([otutable,temp["OTU"]])
+        metabolitetable = np.vstack([metabolitetable,temp["Metabolite"]])
+
+    final = {
+        "OTU": np.matrix(otutable),
+        "Metabolite": np.matrix(metabolitetable) ,
+        "OtuAnn": {"Species":["OTU{}".format(i) for i in range(1,p_otu+1)]},
+        "MetAnn": {"Species":["M{}".format(j) if j<not_mediation_metabolite else "M{} Mediator".format(j) for j in range(1,p_metabolite+1)]}
+        }
+
+    return final
+
+
+class Simulation:
+    def __init__(self,
+        n,
+        p_otu,
+        p_metabolite,
+        multivar=False,
+        A=None,
+        B=None):
+        """
+        n: int, number of simulation samples
+        p_otu: int, number of otu populations
+        p_metabolite: int, number of metabolite populations
+        multivar: bool, flag for whether the otu populations are correlated
+        A: a (p_otu*p_otu) covariance matrix encoding the relationship between otu species; ignored
+        if mutlivar is False; required if multivar is True
+        B: a (p_metabolite*p_metabolite) covariance matrix
+        """
+        self.n=n
+        self.p_otu=p_otu
+        self.p_metabolite=p_metabolite
+        self.multivar=multivar
+        if multivar:
+            self.A=A
+            self.B=B
+            
+        # ? unsure about the metabolite abundance
+        if multivar:
+            self.otutable = np.random.multivariate_normal(np.zeros(p_otu),A,size=(p_otu,n))
+            self.metabolitetable = np.random.multivariate_normal(np.zeros(p_metabolite),B,size=(p_metabolite,n))
+        else:
+            self.otutable = np.random.rand(p_otu,n)
+            self.metabolitetable = np.random.rand(p_metabolite,n)
+    
+    def simulate_abundance(self,
+        mediations=1,
+        b={'b11':-2,'b21':2,'b32':-1.5},
+        mediation_func="simple"):
+        """
+        mediations: int, number of mediations specified for the simulation
+        b: dictionary, truth values for the relationship between producer and target
+        mediation_func: str or callable, the method for generating related producer-target abundance
+        """
+        self.b=b
+
+        if isinstance(mediation_func,str):
+            if mediation_func=="simple_direct": mediation_func=self.__simple_direct
+            elif mediation_func=="simple": mediation_func=self.__simple
+        elif callable(mediation_func):
+            pass
+        else:
+            mediation_func=self.__simple
+
+        self.is_dependent=np.zeros(self.otutable.shape[0])
+        for ix in range(mediations):
+            otu,metabolite=mediation_func(self.p_otu-1-ix,**self.b)
+            self.otutable[ix,:]=otu
+            self.metabolitetable[ix,:]=metabolite
+
+            self.is_dependent[ix]=1
+        
+        return self.otutable,self.metabolitetable
+        
+    def __simple(self,j,b11,b21,b32):
+        """
+        private method for calculating related target & metabolite abundance
+        """
+        producer=self.otutable[j,:]
+        mm=b21*producer+np.random.normal(size=self.n)
+        target=b11*producer+b32*mm+np.random.normal(size=self.n)
+
+        return target,mm
+
+    def __simple_direct(self,j,b11,b21,b32):
+        """
+        private method for calculating directly related target & metabolite abundance
+        """
+        producer=self.otutable[j,:]
+        mm=b21*producer+np.random.normal(size=self.n)
+        target=b32*mm+np.random.normal(size=self.n)
+
+        return target,mm
